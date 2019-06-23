adb805243de2fa16c6e489d77b3aa0825013bb58
Kevin Decherf <kevin@kdecherf.com>
2019-06-23 22:13:06 +0200

    Add IgnoreOriginRule-related entities, db migration, update config
    Add IgnoreOriginUserRule for user-defined rules and
    IgnoreOriginInstanceRule for system-wide rules. Add an interface for
    these two new entities.
    
    Signed-off-by: Kevin Decherf <kevin@kdecherf.com>

---
+++ app/DoctrineMigrations/Version20190826204730.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Wallabag\CoreBundle\Doctrine\WallabagMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190826204730 extends WallabagMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->skipIf($schema->hasTable($this->getTable('ignore_origin_user_rule')), 'It seems that you already played this migration.');
+        $this->skipIf($schema->hasTable($this->getTable('ignore_origin_instance_rule')), 'It seems that you already played this migration.');
+
+        $userTable = $schema->createTable($this->getTable('ignore_origin_user_rule', true));
+        $userTable->addColumn('id', 'integer', ['autoincrement' => true]);
+        $userTable->addColumn('config_id', 'integer');
+        $userTable->addColumn('rule', 'string', ['length' => 255]);
+        $userTable->addIndex(['config_id'], 'idx_config');
+        $userTable->setPrimaryKey(['id']);
+        $userTable->addForeignKeyConstraint($this->getTable('config'), ['config_id'], ['id'], [], 'fk_config');
+
+        $instanceTable = $schema->createTable($this->getTable('ignore_origin_instance_rule', true));
+        $instanceTable->addColumn('id', 'integer', ['autoincrement' => true]);
+        $instanceTable->addColumn('rule', 'string', ['length' => 255]);
+        $instanceTable->setPrimaryKey(['id']);
+
+        if ('postgresql' === $this->connection->getDatabasePlatform()->getName()) {
+            $schema->dropSequence('ignore_origin_user_rule_id_seq');
+            $schema->createSequence('ignore_origin_user_rule_id_seq');
+
+            $schema->dropSequence('ignore_origin_instance_rule_id_seq');
+            $schema->createSequence('ignore_origin_instance_rule_id_seq');
+        }
+    }
+
+    public function postUp(Schema $schema): void
+    {
+        foreach ($this->container->getParameter('wallabag_core.default_ignore_origin_instance_rules') as $entity) {
+            $previous_rule = $this->container
+                ->get('doctrine.orm.default_entity_manager')
+                ->getConnection()
+                ->fetchArray('SELECT * FROM ' . $this->getTable('ignore_origin_instance_rule') . " WHERE rule = '" . $entity['rule'] . "'");
+
+            if (false === $previous_rule) {
+                $this->addSql('INSERT INTO ' . $this->getTable('ignore_origin_instance_rule') . " (rule) VALUES ('" . $entity['rule'] . "');");
+            }
+        }
+    }
+
+    public function down(Schema $schema): void
+    {
+        $schema->dropTable($this->getTable('ignore_origin_user_rule'));
+        $schema->dropTable($this->getTable('ignore_origin_instance_rule'));
+    }
+}
